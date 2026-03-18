e2e5f353705cce314afa74ed6cf3d3c61625941a
Sharukesh308 <230701308@rajalakshmi.edu.in>
2026-03-18 19:55:15 +0530

    Implement MainActivity with fragment navigation

---
+++ MainActivity4.kt
@@ -0,0 +1,33 @@
+package com.example.movieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnBasic = findViewById<Button>(R.id.btnBasic)
+        val btnAdditional = findViewById<Button>(R.id.btnAdditional)
+
+        // Load default fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, MovieBasicDetailsFragment())
+            .commit()
+
+        btnBasic.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, MovieBasicDetailsFragment())
+                .commit()
+        }
+
+        btnAdditional.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, MovieAdditionalDetailsFragment())
+                .commit()
+        }
+    }
+}
